7b49453c9832945c321f696f8dd03ee86657e7ab
Nina <Nina@DESKTOP-B4BPJ7G>
2024-02-24 20:34:36 +0100

    Prueba-Commit

---
+++ PruebaProyectoGit/src/main/java/com/mycompany/pruebaproyectogit/PruebaProyectoGit.java
@@ -0,0 +1,15 @@
+package com.mycompany.pruebaproyectogit;
+
+/**
+ *
+ * @author Nina
+ */
+public class PruebaProyectoGit {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+        System.out.println("************************************************");
+        System.out.println("Esto es una prueba para el repositorio de GitHub");
+    }
+}
