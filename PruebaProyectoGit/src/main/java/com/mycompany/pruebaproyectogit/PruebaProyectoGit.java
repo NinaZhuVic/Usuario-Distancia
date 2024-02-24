d4df8416e003f55474a5c53c673c8b7065201fc7
Nina <Nina@DESKTOP-B4BPJ7G>
2024-02-24 20:56:21 +0100

    Versión_arrayDías

--- PruebaProyectoGit/src/main/java/com/mycompany/pruebaproyectogit/PruebaProyectoGit.java
+++ PruebaProyectoGit/src/main/java/com/mycompany/pruebaproyectogit/PruebaProyectoGit.java
@@ -1,4 +1,5 @@
 package com.mycompany.pruebaproyectogit;
+import java.util.Scanner;
 
 /**
  *
@@ -8,8 +9,23 @@ public class PruebaProyectoGit {
 
     public static void main(String[] args) 
     {
-        System.out.println("Hello World!");
-        System.out.println("************************************************");
-        System.out.println("Esto es una prueba para el repositorio de GitHub");
+        //Creación de Array de días según mes del año.
+        
+        Scanner teclado = new Scanner (System.in);
+        
+        System.out.println("Introduce un número: ");
+        int num = teclado.nextInt();
+        
+        int arrayDias [] = {30, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        for (int i=0; i<=arrayDias.length; i++)
+        {
+            if( i == num)
+            {
+                System.out.print(arrayDias[i+1]);
+            }
+        }
+        
+        
     }
 }
